1c007a817185ee4fbdd8310f07cdac1ea9b4d8c3
lederman <ledermannoam18@gmail.com>
2022-12-31 22:48:14 +0200

    2 commit

---
+++ 1.py
@@ -0,0 +1,14 @@
+is_publiosh = False
+name = "jhon"
+age = 20
+is_new = True
+
+#### user input
+#name = input('what is your name?')
+#print('hi'+ " " + name)
+
+#color = input('what is your color ?')
+#print('my color is ' + '' + color)
+
+
+
---
+++ Type converting.py
@@ -0,0 +1,7 @@
+birth_year = input('birth year: ')
+age = 2022 - int(birth_year)
+print ('your age is :' + '' +str(age))
+
+weight = input('What is your weight in pounds ? ')
+convert_to_Kg = (int(weight)*0.45359237)
+print('your weight in Kg is :'+ '' +str(convert_to_Kg))
