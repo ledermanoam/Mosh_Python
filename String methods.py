18d405f8030add1a4ac6750d95d0fd8aeb4da0ef
lederman <ledermannoam18@gmail.com>
2023-01-09 23:05:55 +0200

    3 commit

---
+++ String methods.py
@@ -0,0 +1,10 @@
+course = 'Python for Beginners'
+print((len(course)))
+print(course.upper())
+print(course.lower())
+
+course.find('P')
+print(course.replace('Beginners', 'Absolute Beginners'))
+print(course)
+
+
