24cd60b6008a6b3e55842c37163ba422b2336467
lederman <ledermannoam18@gmail.com>
2023-02-01 22:55:21 +0200

    Merge remote-tracking branch 'origin/master'

--- formatted Strings.py
+++ formatted Strings.py
@@ -1,3 +1,4 @@
+import datetime
 first = 'john'
 last = 'lederman'
 
@@ -7,3 +8,10 @@ print(meassgae) ## --> this solution is too long
 msg = f'{first} [{last}] is a coder'
 print(msg)
 
+
+
+date = datetime.datetime.now()
+print(date)
+
+msg1= f'{date} is the current date you fucker'
+print(msg1)
