a7176179af15d351e790960f9e1ecf212497d8c2
lederman <ledermannoam18@gmail.com>
2023-03-12 23:11:15 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ comparison operators - example.py
@@ -0,0 +1,8 @@
+name = []
+name = input("Please insert your name")
+if len(name) > 5:
+    print("the name is too long")
+elif len(name) < 3:
+    print("name is ok")
+else:
+    print("go fuck")
---
+++ comparison operators.py
@@ -0,0 +1,8 @@
+# compare
+
+temp = 30
+
+if temp > 30:
+    print("it a hot day")
+else:
+    print("it a not hot day")
