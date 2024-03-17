39f9cb6e88ba05ce1316bd4f079dfbb7c56d34ed
lederman <ledermannoam18@gmail.com>
2024-03-17 22:01:47 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ Tuple.py
@@ -0,0 +1,13 @@
+### tuple == list without option to change or update ###
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+### print item from the tuple ###
+
+print(thistuple[2])
+
+### tuple length ###
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
