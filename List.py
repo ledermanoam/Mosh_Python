d58dbd1c5a5067845c8a60a25f9c766cd1470bca
lederman <ledermannoam18@gmail.com>
2024-03-17 22:18:21 +0200

    Merge remote-tracking branch 'origin/master'

--- List.py
+++
@@ -1,8 +0,0 @@
-### simple list
-
-mylist = ["apple", "banana", "cherry"]
-print(mylist)
-
-### print item from the list
-
-print(mylist[2])
---
+++ list and dictionaries.py
@@ -0,0 +1,31 @@
+### simple list
+
+mylist = ["apple", "banana", "cherry"]
+print(mylist)
+
+### print item from the list
+
+print(mylist[2])
+
+### List with Dictionary ###
+
+dic = {"a":1,"b":2,"c":3,"d":4}
+print(dic["a"])
+
+### create Dictionary ###
+dic1 = {"a":2,4:"bcd","c":"hello world"}
+print(dic1[4])
+print(dic1["c"])
+
+### add items to the Dictionary ###
+
+new_dic = {}  ## creating a new dictionary
+new_dic["firstname"] = "ido" ## add items to the dictionary
+new_dic["lastname"] = "lederman"
+new_dic["gender"] = "male"
+print(new_dic)
+
+
+
+
+
